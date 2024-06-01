f6f0f0ea2b1cf3f64413c79e785039566ad20499
ashavan33 <105936390+ashavan33@users.noreply.github.com>
2024-06-01 08:44:06 -0400

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'dropsquadproductions.com', 'www.dropsquadproductions.com'
+    origins 'dropsquadproductions.com', 'www.dropsquadproductions.com', 'localhost'
 
 
     resource "*",
